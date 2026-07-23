c1c14da51bf60c59fd9404854a1ca41a96d4d440
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-07-23 13:47:20 +0530

    feat: implement update, delete, activate, and deactivate functionalities in ProductController and update routes

--- backend/src/controllers/ProductController.ts
+++ backend/src/controllers/ProductController.ts
@@ -63,4 +63,104 @@ const getAllProducts = async (
   }
 };
 
-export { addProduct, getAllProducts };
+const updateProduct = async (
+  req: Request<{ id: string }, {}, ProductReq>,
+  res: Response<ProductRes<Product>>,
+) => {
+  try {
+    const { id } = req.params;
+    const { name, price, baseunit, description } = req.body;
+
+    if (!name || !price || !baseunit || !description) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (price <= 0) {
+      return res
+        .status(400)
+        .json({ message: "Price must be greater than zero" });
+    }
+
+    const product = await prisma.product.update({
+      where: { id },
+      data: {
+        name,
+        price,
+        baseunit,
+        description,
+      },
+    });
+
+    res
+      .status(200)
+      .json({ message: "Product updated successfully", data: product });
+  } catch (error) {
+    console.error("updateProduct error:", error);
+    res.status(500).json({ message: "update product server error" });
+  }
+};
+
+const deleteProduct = async (
+  req: Request<{ id: string }>,
+  res: Response<ProductRes<null>>,
+) => {
+  try {
+    const { id } = req.params;
+
+    await prisma.product.delete({
+      where: { id },
+    });
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.error("deleteProduct error:", error);
+    res.status(500).json({ message: "delete product server error" });
+  }
+};
+
+const activateProduct = async (
+  req: Request<{ id: string }>,
+  res: Response<ProductRes<Product>>,
+) => {
+  try {
+    const { id } = req.params;
+    const product = await prisma.product.update({
+      where: { id },
+      data: { status: "Active" },
+    });
+
+    res
+      .status(200)
+      .json({ message: "Product activated successfully", data: product });
+  } catch (error) {
+    console.error("activateProduct error:", error);
+    res.status(500).json({ message: "activate product server error" });
+  }
+};
+
+const deactivateProduct = async (
+  req: Request<{ id: string }>,
+  res: Response<ProductRes<Product>>,
+) => {
+  try {
+    const { id } = req.params;
+    const product = await prisma.product.update({
+      where: { id },
+      data: { status: "Inactive" },
+    });
+    res
+      .status(200)
+      .json({ message: "Product deactivated successfully", data: product });
+  } catch (error) {
+    console.error("deactivateProduct error:", error);
+    res.status(500).json({ message: "deactivate product server error" });
+  }
+};
+
+export {
+  addProduct,
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+  activateProduct,
+  deactivateProduct,
+};
