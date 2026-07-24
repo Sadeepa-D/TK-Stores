41b3b7a36dbb8bdc0f868509dbda5656d9c2ddaf
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-07-24 18:49:25 +0530

    feat: add additional unit types to Unit enum and create migration for database update

---
+++ backend/prisma/migrations/20260724131839_add_more_unittypes_toproducts/migration.sql
@@ -0,0 +1,13 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "Unit" ADD VALUE 'G';
+ALTER TYPE "Unit" ADD VALUE 'MILILITER';
+ALTER TYPE "Unit" ADD VALUE 'METERS';
+ALTER TYPE "Unit" ADD VALUE 'CENTIMETERS';
+ALTER TYPE "Unit" ADD VALUE 'PACKETS';
